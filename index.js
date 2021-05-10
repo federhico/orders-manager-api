2506d5609db70145e345d59232bd86be129589a4
AgusGelos <juanagustingelos1@gmail.com>
2021-05-10 09:36:35 -0300

    console log port added

--- index.js
+++ index.js
@@ -6,11 +6,19 @@ UBICAMOS LOS REQUERIMIENTOS
 const express = require('express');
 const bodyParser = require('body-parser');
 
+// const mongoose = require('mongoose');
+
 const app = express()
 const port = process.env.PORT || 3001
 
 app.use(bodyParser.urlencoded({extended : false}))
 app.use(bodyParser.json())
+
+app.listen(port, () =>{
+    console.log(
+       `Listening on port: ${port}`
+     )
+})
 
 /*=============================================
 IMPORTAMOS LAS RUTAS
